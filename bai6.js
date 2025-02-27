2fdee6967311f1fdd438df2095422ab9fb494e24
davidpham26 <phonghaha20065432@gmail.com>
2025-02-27 07:27:30 +0700

    bai6.js

--- bai6.js
+++ bai6.js
@@ -1,5 +1,5 @@
 let monney = prompt("Nhập tiền Đô:");
 let usd = parseFloat(monney);
 let usdToVnd = usd * 25000;
-document.write(`${usd} USD = ${usdToVnd.toLocaleString()} VND<br>`);
-document.write(`${vnd} VND = ${(vnd / 25000).toFixed(2)} USD`);
+let vnd = usdToVnd
+document.write(usdToVnd);
